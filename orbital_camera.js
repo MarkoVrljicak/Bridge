edf727f74811236ba83bbe4d2a20000bf790267e
MarkoVrljicak <marcos.vrljicak@gmail.com>
2016-10-11 14:54:15 -0300

    OrbitalCamera corrects for displacement due to discreteness of movement along the tangent vector.

--- orbital_camera.js
+++ orbital_camera.js
@@ -1,6 +1,7 @@
 function OrbitalCamera(){
+    this.constant_hight = 75;
     this.eye_point = vec3.create();
-    vec3.set(this.eye_point, 40, 75, -100);
+    vec3.set(this.eye_point, 40, this.constant_hight, -100);
     this.at_point = vec3.create();
     vec3.set(this.at_point, 0, 0, 0);
     this.up_point = vec3.create();
@@ -17,11 +18,24 @@ function OrbitalCamera(){
     };
 
     this.orbit = function(speed){
+        var orbit_radius_vector = vec2.create();
+        orbit_radius_vector[0] = this.eye_point[0];
+        orbit_radius_vector[1] = this.eye_point[2];
+        var orbit_radius_length = vec2.length(orbit_radius_vector);
         // Move
         var direction = vec3.create();
         vec3.cross(direction, this.eye_point, this.up_point);
         vec3.normalize(direction, direction);
         vec3.add(this.eye_point, vec3.scale(direction, direction, speed), this.eye_point);
+        // Correct
+        this.eye_point[1] = this.constant_hight;
+        var uncorrected = vec2.create();
+        uncorrected[0] = this.eye_point[0];
+        uncorrected[1] = this.eye_point[2];
+        var scale_factor = orbit_radius_length/vec2.length(uncorrected);
+        vec2.scale(uncorrected, uncorrected, scale_factor);
+        this.eye_point[0] = uncorrected[0];
+        this.eye_point[2] = uncorrected[1];
     };
 
     this.onWheel = function(e) {
